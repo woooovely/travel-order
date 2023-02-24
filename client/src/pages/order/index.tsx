83c40748648395f07810846986c6f67b4f53d9d8
조우상 <woosang3078@hanmail.net>
2023-02-24 15:41:08 +0900

    ⚡ :: 옵션 컴포넌트 모듈 할당

--- client/src/pages/order/index.tsx
+++ client/src/pages/order/index.tsx
@@ -1,12 +1,36 @@
 import React, { useContext } from "react";
 import styled from 'styled-components'
+import Type from "../../components/type";
+import { OrderContext } from "../../context";
 
 const OrderPage = () => {
+    const [orderData] = useContext<any>(OrderContext);
+
     return (
         <div>
             <h1>Travel Products</h1>
+            <div>
+                <Type orderType="products" />
+            </div>
+            <OptionContainer>
+                <Options>
+                    <Type orderType="options" />
+                </Options>
+                <Options>
+                    <h2>총 합계: {orderData.totals.total}</h2>
+                </Options>
+            </OptionContainer>
         </div>
     )
 }
 
 export default OrderPage;
+
+const Options = styled.div`
+    width: 50%;
+`
+
+const OptionContainer = styled.div`
+    display: flex;
+    margin-top: 20px;
+`
