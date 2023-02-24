5df090f9fd6af53e2fd247cb59409411682a051e
조우상 <woosang3078@hanmail.net>
2023-02-24 13:10:40 +0900

    ⚡ :: Context API 할당

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { OrderContextProvider } from './context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <OrderContextProvider>
+      <App />
+    </OrderContextProvider>
   </React.StrictMode>
 );
 
