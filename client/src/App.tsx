f66fcfc617c8dbad3d4b3a995701489ec00ab258
조우상 <woosang3078@hanmail.net>
2023-02-24 15:57:06 +0900

    ⚡ :: 라우터 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
 import OrderPage from "./pages/order";
+import SummaryPage from "./pages/summary";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<OrderPage />}></Route>
+        <Route path="/check" element={<SummaryPage />}></Route>
       </Routes>
     </BrowserRouter>
   )
