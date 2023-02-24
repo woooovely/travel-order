a60caabd9adfc128808c54f9f99da582d200a85e
조우상 <woosang3078@hanmail.net>
2023-02-24 10:21:43 +0900

    ⚡ :: 주문하기 페이지 추가

---
+++ client/src/pages/order/index.tsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import styled from 'styled-components'
+
+const OrderPage = () => {
+    return (
+        <div>
+            <h1>Travel Products</h1>
+        </div>
+    )
+}
+
+export default OrderPage;
