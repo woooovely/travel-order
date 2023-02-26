45ba201c893d2fd1ef1070781192133fa5740077
woosang <woosang3078@hanmail.net>
2023-02-26 17:54:06 +0900

    ⚡ :: 주문 완료 페이지 추가

---
+++ client/src/pages/complete/index.tsx
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react";
+import styled from "styled-components";
+import axios from "axios";
+import { OrderContext } from "../../context";
+
+const CompletePage = () => {
+  const [orderHistory, setOrderHistory] = useState([]);
+  const [orderData] = useContext(OrderContext);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    orderCompleted(orderData);
+  }, [orderData]);
+
+  const orderCompleted = async (orderData: any) => {
+    try {
+      const res = await axios.post("http://localhost:4000/order", orderData);
+      console.log('response', res);
+      setOrderHistory(res.data);
+      setLoading(true);
+    } catch (error) {
+        console.log(error);
+    }
+  };
+
+  const showOrder = orderHistory.map((item: any, key: any) => (
+    <tr key={item.orderNumber}>
+        <td>{item.orderNumber}</td>
+        <td>{item.title}</td>
+    </tr>
+  ))
+};
