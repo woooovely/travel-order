d2dc298e881879dfa6dea526f48c73f4c5befbd5
woosang <woosang3078@hanmail.net>
2023-03-01 21:51:12 +0900

    ⚡ :: 소개 문구 추가

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const MainPage = () => {
     return (
-        <div>This is Main Page.</div>
+        <>
+          <TopSpace />
+          <TextContainer>
+            <p>당신만의<br />특별한 여행을<br />주문하고 즐겨보세요</p>
+          </TextContainer>
+        </>
     )
 }
 
 export default MainPage;
+
+const TopSpace = styled.div`
+    height: 400px;
+`
+
+const TextContainer = styled.div`
+    margin-left: 50px;
+    p {
+        font-size: 70px;
+        font-weight: 600;
+    }
+`
