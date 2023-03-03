07acb1494b3af24fcd07a416103a317d6cef95e3
woosang <woosang3078@hanmail.net>
2023-03-03 23:48:08 +0900

    ♻️ :: context data 수정

--- client/src/components/type.tsx
+++ client/src/components/type.tsx
@@ -13,7 +13,7 @@ interface OrderProps {
 const Type = ({ orderType }: OrderProps) => {
   const [items, setItems] = useState<string[] | number[]>([]);
   const [error, setError] = useState<boolean>(false);
-  const [orderData, updateItemCount] = useContext<any>(OrderContext);
+  const [updateItemCount] = useContext<any>(OrderContext);
 
   useEffect(() => {
     loadItems(orderType);
