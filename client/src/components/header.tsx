1fbc6f89771597b64ad6e7d7cf61549b0ca7f102
woosang <woosang3078@hanmail.net>
2023-03-06 08:22:45 +0900

    ⚡ :: 헤더 추가

---
+++ client/src/components/header.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = () => {
+
+    return (
+        <>
+          <HeaderContainer>
+            <LogoContainer>
+                <LogoText>Your Travels</LogoText>
+            </LogoContainer>
+            <div>
+                <CategoryContainer>
+                    <Categories>홈</Categories>
+                    <Categories>상품 조회</Categories>
+                    <Categories>주문하기</Categories>
+                </CategoryContainer>
+            </div>
+          </HeaderContainer>
+        </>
+    )
+}
+
+export default Header;
+
+const Categories = styled.li`
+    
+`
+
+const CategoryContainer = styled.ul`
+    list-style: none;
+    display: flex;
+    justify-content: center;
+    margin-top: -50px;
+`
+
+const LogoContainer = styled.div`
+    padding: 5px;
+    margin-left: 20px;
+`
+
+const LogoText = styled.p`
+    font-size: 25px;
+    font-weight: 550;
+`
+
+const HeaderContainer = styled.header`
+    height: 90px;
+    border-bottom: 1px solid #B6B6B6;
+`
