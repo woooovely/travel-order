8cd10225c65114c83caf3816d2a7a46e1d88436c
woosang <woosang3078@hanmail.net>
2023-03-06 08:43:58 +0900

    ⚡ :: 페이지 탭 제목 할당

--- client/src/pages/lists/index.tsx
+++ client/src/pages/lists/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
+import Header from '../../components/header';
+import useTitle from '../../hook/title';
 
 const ProductLists = () => {
-    
+    const titleUpdater = useTitle('불러오는 중...')
+    setTimeout(() => titleUpdater('상품 조회 | Your Travels'))
+
     return (
         <div>
-            
+            <Header />
         </div>
     )
 }
+
+export default ProductLists;
