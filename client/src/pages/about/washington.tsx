af5e792a2f70d3a52c90c719450ffaab03d9935d
woosang <woosang3078@hanmail.net>
2023-03-07 17:15:49 +0900

    ⚡ :: 미국 워싱턴 D.C. 상품 조회 페이지 추가

---
+++ client/src/pages/about/washington.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/header';
+
+const AboutWashington = () => {
+    return (
+        <Header />
+    )
+}
+
+export default AboutWashington;
