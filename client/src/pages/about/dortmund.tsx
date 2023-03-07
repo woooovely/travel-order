c1ad1c3693b8651335dc81e99ec6e34fedebe453
woosang <woosang3078@hanmail.net>
2023-03-07 17:24:27 +0900

    ⚡ :: 독일 도르트문트 상품 조회 페이지 추가

---
+++ client/src/pages/about/dortmund.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/header';
+
+const AboutDortmund = () => {
+    return (
+        <Header />
+    )
+}
+
+export default AboutDortmund;
