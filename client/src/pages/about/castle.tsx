19dead544793972e638835543d38c1fc07ecceec
woosang <woosang3078@hanmail.net>
2023-03-07 17:27:34 +0900

    ⚡ :: 포틀랜드 캐슬타운 상품 조회 페이지 추가

---
+++ client/src/pages/about/castle.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/header';
+
+const AboutCastleTown = () => {
+    return (
+        <Header />
+    )
+}
+
+export default AboutCastleTown;
