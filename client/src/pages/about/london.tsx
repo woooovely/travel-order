4aa4d4e90d565cdbf2dbb3a55a7f572a45939bc1
woosang <woosang3078@hanmail.net>
2023-03-07 17:20:16 +0900

    ⚡ :: 영국 런던 상품 조회 페이지 추가

---
+++ client/src/pages/about/london.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import styled from 'styled-components';
+import Header from '../../components/header';
+
+const AboutLondon = () => {
+    return (
+        <Header />
+    )
+}
+
+export default AboutLondon;
