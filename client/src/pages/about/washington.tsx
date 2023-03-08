36d2108afb75052281e3ecfd331bc98f8cebf932
woosang <woosang3078@hanmail.net>
2023-03-08 09:20:05 +0900

    ⚡ :: 상품 제목 추가

--- client/src/pages/about/washington.tsx
+++ client/src/pages/about/washington.tsx
@@ -4,8 +4,18 @@ import Header from '../../components/header';
 
 const AboutWashington = () => {
     return (
-        <Header />
+        <>
+          <Header />
+          <div>
+            <ProductTitle>미국 | 위싱턴 D.C. 패키지</ProductTitle>
+          </div>
+        </>
     )
 }
 
 export default AboutWashington;
+
+const ProductTitle = styled.p`
+    font-size: 35px;
+    font-weight: 600;
+`
