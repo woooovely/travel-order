254f618e5c6d7ede0afe7fe3591e21faf7844515
woosang <woosang3078@hanmail.net>
2023-03-08 16:19:08 +0900

    ⚡ :: 인덱스 시그니처 추가

--- client/src/types/show.ts
+++ client/src/types/show.ts
@@ -1,5 +1,11 @@
-export interface ShowProps {
-    name: string
-    imagePath: string
-    url: string
+export interface NameProps {
+    [name: string]: string
+}
+
+export interface ImageProps {
+    [imagePath: string]: string
+}
+
+export interface UrlProps {
+    [url: string]: string
 }
