b383007740bdad02809904cacb309c049bb079b8
woosang <woosang3078@hanmail.net>
2023-03-10 11:15:53 +0900

    ⚡ :: 외부 인터페이스 수정

--- client/src/types/show.ts
+++ client/src/types/show.ts
@@ -1,11 +1,5 @@
-export interface NameProps {
-    [name: string]: string
-}
-
-export interface ImageProps {
-    [imagePath: string]: string
-}
-
-export interface UrlProps {
-    [url: string]: string
+export interface ShowProps {
+    name: string
+    imagePath: string
+    url: string
 }
