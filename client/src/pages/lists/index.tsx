3b770207f914434fb0fbc040f9dcdf99e29c21b1
woosang <woosang3078@hanmail.net>
2023-03-10 12:10:47 +0900

    ♻️ :: 미선언 코드 제거

--- client/src/pages/lists/index.tsx
+++ client/src/pages/lists/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/header";
 import useTitle from "../../hook/title";
