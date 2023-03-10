b1aed70e48bd69374789c301b3dc6e159271361b
woosang <woosang3078@hanmail.net>
2023-03-10 11:14:01 +0900

    ⚡ :: query client 컴포넌트 할당

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "react-router-dom";
 import { Route } from "react-router-dom";
@@ -12,30 +13,34 @@ import MainPage from "./pages/main";
 import OrderPage from "./pages/order";
 import SummaryPage from "./pages/summary";
 
+const queryClient = new QueryClient();
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />}></Route>
-        <Route path="lists" element={<ProductLists />}></Route>
-        <Route path="/order" element={<OrderPage />}></Route>
-        <Route path="/check" element={<SummaryPage />}></Route>
-        <Route path="/complete" element={<CompletePage />}></Route>
-        <Route
-          path="/about/usa-washington"
-          element={<AboutWashington />}
-        ></Route>
-        <Route path="/about/uk-london" element={<AboutLondon />}></Route>
-        <Route
-          path="/about/germany-dortmund"
-          element={<AboutDortmund />}
-        ></Route>
-        <Route
-          path="/about/portland-castletown"
-          element={<AboutCastleTown />}
-        ></Route>
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />}></Route>
+          <Route path="lists" element={<ProductLists />}></Route>
+          <Route path="/order" element={<OrderPage />}></Route>
+          <Route path="/check" element={<SummaryPage />}></Route>
+          <Route path="/complete" element={<CompletePage />}></Route>
+          <Route
+            path="/about/usa-washington"
+            element={<AboutWashington />}
+          ></Route>
+          <Route path="/about/uk-london" element={<AboutLondon />}></Route>
+          <Route
+            path="/about/germany-dortmund"
+            element={<AboutDortmund />}
+          ></Route>
+          <Route
+            path="/about/portland-castletown"
+            element={<AboutCastleTown />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 };
 
