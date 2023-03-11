06152d0c05011a86f168e60a1caa7a778efabc43
woosang <woosang3078@hanmail.net>
2023-03-11 22:04:35 +0900

    ⚡ :: 이미지 간격 할당

--- client/src/pages/about/washington.tsx
+++ client/src/pages/about/washington.tsx
@@ -1,11 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/header";
 import useTitle from "../../hook/title";
+import { useQuery } from "react-query";
+import axios from "axios";
 
 const AboutWashington = () => {
   const titleUpdater = useTitle("불러오는 중...");
   setTimeout(() => titleUpdater("미국 워싱턴 D.C. 7일 | Your Travels"));
+
+  const [items, setItems] = useState<string[]>([]);
+
+  const useLoadImages = () => {
+    const { isLoading, error, isError }: any = useQuery(
+      "load-image",
+      async () => {
+        const response = await axios.get("http://localhost:4000/products");
+        setItems(response.data);
+      }
+    );
+
+    if (isLoading) {
+      return <h2>로딩중...</h2>;
+    }
+
+    if (isError) {
+      return <h2>{error.message}</h2>;
+    }
+  };
+
+  useLoadImages();
 
   return (
     <>
@@ -17,6 +41,7 @@ const AboutWashington = () => {
           <Details>독립 행정구</Details>
           <Details>인기 상품</Details>
         </ProductDetail>
+        <Images src="http://localhost:4000/images/america.jpeg" />
       </ProductContainer>
     </>
   );
@@ -24,10 +49,20 @@ const AboutWashington = () => {
 
 export default AboutWashington;
 
-const Details = styled.li``;
-
-const ProductDetail = styled.ul`
+const Images = styled.img`
+  width: 70%;
   display: flex;
+  padding-left: 400px;
+`;
+
+const Details = styled.li`
+  margin-left: 20px;
+`;
+
+const ProductDetail = styled.p`
+  display: flex;
+  list-style: none;
+  padding-right: 15px;
 `;
 
 const ProductContainer = styled.div`
@@ -35,11 +70,12 @@ const ProductContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  margin-top: 10px;
+  margin-top: 30px;
   flex-direction: column;
 `;
 
 const ProductTitle = styled.p`
   font-size: 25px;
   font-weight: 600;
+  margin: 0;
 `;
