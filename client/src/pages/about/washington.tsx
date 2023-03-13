95a2aab27f292880796fe95784c66252e9036adf
woosang <woosang3078@hanmail.net>
2023-03-13 11:21:19 +0900

    ⚡ :: 설명 문구 추가

--- client/src/pages/about/washington.tsx
+++ client/src/pages/about/washington.tsx
@@ -42,12 +42,34 @@ const AboutWashington = () => {
           <Details>인기 상품</Details>
         </ProductDetail>
         <Images src="http://localhost:4000/images/america.jpeg" />
+        <MidSpace />
+        <DetailTitle>설명</DetailTitle>
+        <Description>
+          워싱턴 D.C.는 미국의 수도로 백악관과 국회의사당, 연방 대법원을 포함한
+          많은 연방 정부의 주요 관청이 있으며 174개 국가의 대사관과 국제통화기금
+          본부 등 각종 국제기구의 본부가 있는 국제 정치와 외교의 중심지이다.
+        </Description>
       </ProductContainer>
     </>
   );
 };
 
 export default AboutWashington;
+
+const Description = styled.div`
+  width: 500px;
+  margin-left: 50px;
+`;
+
+const DetailTitle = styled.p`
+  font-size: 23px;
+  margin-right: 240px;
+  font-weight: 600;
+`;
+
+const MidSpace = styled.div`
+  height: 50px;
+`;
 
 const Images = styled.img`
   width: 70%;
