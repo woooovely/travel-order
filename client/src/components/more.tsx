1b922edcec764b22428e2af5d6574ac6c76377e0
woosang <woosang3078@hanmail.net>
2023-03-13 12:00:02 +0900

    ⚡ :: 다른 상품 보기 컴포넌트 추가

---
+++ client/src/components/more.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import { useQuery } from 'react-query';
+import axios from 'axios';
+
+const MoreProducts = () => {
+    const loadImages = () => {
+
+    }
+}
+
+export default MoreProducts;
