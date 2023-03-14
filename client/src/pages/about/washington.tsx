bb0d1eae08d6de726dce49d4233273c1f34505cc
woosang <woosang3078@hanmail.net>
2023-03-14 16:47:24 +0900

    ♻️ :: 자원낭비 코드 제거

--- client/src/pages/about/washington.tsx
+++ client/src/pages/about/washington.tsx
@@ -1,35 +1,11 @@
-import { useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/header";
 import useTitle from "../../hook/title";
-import { useQuery } from "react-query";
-import axios from "axios";
+
 
 const AboutWashington = () => {
   const titleUpdater = useTitle("불러오는 중...");
   setTimeout(() => titleUpdater("미국 워싱턴 D.C. 7일 | Your Travels"));
-
-  const [items, setItems] = useState<string[]>([]);
-
-  const useLoadImages = () => {
-    const { isLoading, error, isError }: any = useQuery(
-      "load-image",
-      async () => {
-        const response = await axios.get("http://localhost:4000/products");
-        setItems(response.data);
-      }
-    );
-
-    if (isLoading) {
-      return <h2>로딩중...</h2>;
-    }
-
-    if (isError) {
-      return <h2>{error.message}</h2>;
-    }
-  };
-
-  useLoadImages();
 
   return (
     <>
