701dcbda9c39e288a9cc220b441e9f5abd399551
woosang <woosang3078@hanmail.net>
2023-03-15 14:46:49 +0900

    ⚡ :: 상품 제목 추가

--- client/src/pages/about/dortmund.tsx
+++ client/src/pages/about/dortmund.tsx
@@ -1,10 +1,66 @@
-import styled from 'styled-components';
-import Header from '../../components/header';
+import styled from "styled-components";
+import Header from "../../components/header";
 
 const AboutDortmund = () => {
-    return (
-        <Header />
-    )
-}
+  return (
+    <>
+      <Header />
+      <ProductContainer>
+        <ProductContainer>독일 | 도르트문트 패키지</ProductContainer>   
+      </ProductContainer>
+    </>
+  );
+};
 
 export default AboutDortmund;
+
+const Space = styled.div`
+  height: 120px;
+`;
+
+const Description = styled.div`
+  width: 500px;
+  margin-left: 210px;
+  font-size: 19px;
+`;
+
+const DetailTitle = styled.p`
+  font-size: 23px;
+  margin-right: 240px;
+  font-weight: 600;
+`;
+
+const MidSpace = styled.div`
+  height: 50px;
+`;
+
+const Images = styled.img`
+  width: 70%;
+  display: flex;
+  padding-left: 400px;
+`;
+
+const Details = styled.li`
+  margin-left: 20px;
+`;
+
+const ProductDetail = styled.p`
+  display: flex;
+  list-style: none;
+  padding-right: 15px;
+`;
+
+const ProductContainer = styled.div`
+  width: 1000px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 30px;
+  flex-direction: column;
+`;
+
+const ProductTitle = styled.p`
+  font-size: 25px;
+  font-weight: 600;
+  margin: 0;
+`;
