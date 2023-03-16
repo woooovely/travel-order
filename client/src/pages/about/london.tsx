3c2a397eaf912d6a2691f42090ac66e0b88981fa
woosang <woosang3078@hanmail.net>
2023-03-16 09:16:29 +0900

    ⚡ :: 상품 이미지 미리보기 옵션 추가

--- client/src/pages/about/london.tsx
+++ client/src/pages/about/london.tsx
@@ -16,15 +16,18 @@ const AboutLondon = () => {
           <Details>인기 상품</Details>
           <Details>북런던</Details>
         </ProductDetail>
-        <Images src="http://localhost:4000/images/england.jpeg" />
+        <Images
+          src="http://localhost:4000/images/england.jpeg"
+          alt="상품 이미지"
+        />
         <MidSpace />
         <DetailTitle>설명</DetailTitle>
         <Description>
-          런던(London)은 잉글랜드, 나아가 영국의 수도이자 최대도시이다.
-          영국의 정치, 경제, 문화, 교통의 중심지이면서 전세계, 특히 영연방
-          국가들에도 큰 영향력을 끼친다. 뉴욕, 도쿄와 함께 세계 3대 도시로
-          불리기도 한다. 또한 런던은 다문화 도시로, 심지어 몇몇 기사에서는
-          뉴욕시보다도 좀 더 많은 다양한 집단이 모여사는 도시로 꼽히기도 한다.
+          런던(London)은 잉글랜드, 나아가 영국의 수도이자 최대도시이다. 영국의
+          정치, 경제, 문화, 교통의 중심지이면서 전세계, 특히 영연방 국가들에도
+          큰 영향력을 끼친다. 뉴욕, 도쿄와 함께 세계 3대 도시로 불리기도 한다.
+          또한 런던은 다문화 도시로, 심지어 몇몇 기사에서는 뉴욕시보다도 좀 더
+          많은 다양한 집단이 모여사는 도시로 꼽히기도 한다.
         </Description>
       </ProductContainer>
     </>
