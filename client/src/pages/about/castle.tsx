21faa022358ec8a3459ccc6e639dd82a82a3aa6e
woosang <woosang3078@hanmail.net>
2023-03-17 15:36:43 +0900

    ♻️ :: 미선언 코드 제거

--- client/src/pages/about/castle.tsx
+++ client/src/pages/about/castle.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import styled from 'styled-components';
 import Header from '../../components/header';
 
 const AboutCastleTown = () => {
     return (
-        <Header />
+        <>
+          
+        </>
     )
 }
 
