0c1a5a922c667b7457ebcad066f885682aee185e
woosang <woosang3078@hanmail.net>
2023-03-17 15:39:00 +0900

    ⚡ :: 헤더 추가

--- client/src/pages/about/castle.tsx
+++ client/src/pages/about/castle.tsx
@@ -4,9 +4,11 @@ import Header from '../../components/header';
 const AboutCastleTown = () => {
     return (
         <>
-          
+          <Header />
+
         </>
     )
 }
 
 export default AboutCastleTown;
+
